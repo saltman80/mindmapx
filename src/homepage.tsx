d39bbaf5402a90f8a40862199d79ab2d943cf139
Shawn Altman <saltman80@gmail.com>
2025-07-17 16:59:35 -0700

    Add minimal homepage component

---
+++ src/homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>🚀 Welcome to ScalerMax</h1>
+      <p>This is your homepage. Customize it inside <code>homepage.tsx</code>.</p>
+    </div>
+  )
+}
+
+export default Homepage
