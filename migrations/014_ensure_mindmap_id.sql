e73952537c46373244d8954d14fe920198b7bb7f
Shawn Altman <saltman80@gmail.com>
2025-07-16 23:00:11 -0700

    Remove IF EXISTS from ALTER TABLE

--- migrations/014_ensure_mindmap_id.sql
+++ migrations/014_ensure_mindmap_id.sql
@@ -1,8 +1,8 @@
 -- Ensure mindmap_id column exists in core tables
-ALTER TABLE IF EXISTS nodes
+ALTER TABLE nodes
   ADD COLUMN IF NOT EXISTS mindmap_id UUID REFERENCES mindmaps(id) ON DELETE CASCADE;
 
-ALTER TABLE IF EXISTS todos
+ALTER TABLE todos
   ADD COLUMN IF NOT EXISTS mindmap_id UUID REFERENCES mindmaps(id) ON DELETE CASCADE;
 
 CREATE INDEX IF NOT EXISTS idx_nodes_mindmap_id ON nodes(mindmap_id);
