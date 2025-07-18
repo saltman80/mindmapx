e58801a68ade185c7737dbd8f92e1d9855649f1c
Shawn Altman <saltman80@gmail.com>
2025-07-17 21:47:22 -0700

    Add immutable dep and mark as external

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // ✅ use '/' unless you're deploying under a subpath
+  build: {
+    rollupOptions: {
+      external: ['immutable'],
+    },
+  },
 })
