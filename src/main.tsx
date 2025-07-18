7b34832586d5051c36090f190bdf7c6c32223b4c
Shawn Altman <saltman80@gmail.com>
2025-07-17 20:00:11 -0700

    fix: restore app entry and build config

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-console.log('main.tsx: script start')
-
-const root = document.getElementById('root')
-if (!root) {
-  console.error('Missing #root div in index.html')
-} else {
-  console.log('main.tsx: rendering App')
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
