f9d30ca81d10ade015403189255dfa566b55676a
Shawn Altman <saltman80@gmail.com>
2025-07-26 22:16:31 -0700

    Fix Vite config imports

--- vite.js
+++ vite.js
@@ -1,3 +1,7 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
