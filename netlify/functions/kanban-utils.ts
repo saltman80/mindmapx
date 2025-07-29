6fbaa14fa50fba96fd78151fd5b73b1b93a45379
Shawn Altman <saltman80@gmail.com>
2025-07-28 22:26:08 -0700

    fix new column return type

--- netlify/functions/kanban-utils.ts
+++ netlify/functions/kanban-utils.ts
@@ -1,17 +1,24 @@
 import type { PoolClient } from 'pg'
 
-export async function ensureNewColumn(client: PoolClient, boardId: string): Promise<string> {
-  let { rows } = await client.query(
+export async function ensureNewColumn(
+  client: PoolClient,
+  boardId: string
+): Promise<string> {
+  const { rows } = await client.query(
     "SELECT id FROM kanban_columns WHERE board_id=$1 AND lower(title)='new'",
     [boardId]
   )
-  let newColId = rows[0]?.id as string | undefined
-  if (!newColId) {
+
+  let newColId: string
+  if (rows[0]?.id) {
+    newColId = rows[0].id as string
+  } else {
     const res = await client.query(
       "INSERT INTO kanban_columns (board_id, title, position) VALUES ($1,'New',0) RETURNING id",
       [boardId]
     )
-    newColId = res.rows[0].id
+    newColId = res.rows[0].id as string
   }
+
   return newColId
 }
